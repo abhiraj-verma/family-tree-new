c271204363befd9f706e0194c0bd1dcfaf752938
ash-121999 <92639744+ash-121999@users.noreply.github.com>
2025-07-08 23:26:53 +0530

    Updated FamilyTreeView.tsx

--- components/tree/FamilyTreeView.tsx
+++ components/tree/FamilyTreeView.tsx
@@ -1,19 +1,20 @@
-@@ .. @@
- interface FamilyTreeViewProps {
+interface FamilyTreeViewProps {
    onAddMember: (parentId?: string, relationship?: string) => void
 +  onEditMember?: (memberId: string) => void
- }
+}
 
--export default function FamilyTreeView({ onAddMember }: FamilyTreeViewProps) {
 +export default function FamilyTreeView({ onAddMember, onEditMember }: FamilyTreeViewProps) {
    const { family } = useFamily()
    const [viewMode, setViewMode] = useState<'canvas' | 'grid'>('canvas')
-@@ .. @@
-         {/* Tree Visualization */}
-         <div className="p-6">
-           {viewMode === 'canvas' ? (
--            <FamilyTreeCanvas onAddMember={onAddMember} />
-+            <FamilyTreeCanvas onAddMember={onAddMember} onEditMember={onEditMember} />
-           ) : (
-             <div className="bg-gradient-to-b from-gray-50 to-white p-6 rounded-xl">
-               <GridView />
+
+        {/* Tree Visualization */}
+        <div className="p-6">
+          {viewMode === 'canvas' ? (
+-            <FamilyTreeCanvas onAddMember={onAddMember} onEditMember={onEditMember} />
+          ) : (
+            <div className="bg-gradient-to-b from-gray-50 to-white p-6 rounded-xl">
+              <GridView />
+            </div>
+          )}
+        </div>
+}
